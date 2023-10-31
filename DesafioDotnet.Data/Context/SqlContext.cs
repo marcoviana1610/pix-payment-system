5baa4caffe6385216898c8eb9452f293f46e5f9a
“marcoviana1610” <marcoviana1610@gmail.com>
2023-10-31 13:24:02 -0300

    creating db context

---
+++ DesafioDotnet.Data/Context/SqlContext.cs
@@ -0,0 +1,20 @@
+﻿using DesafioDotnet.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioDotnet.Data.Context
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext(DbContextOptions options): base(options) 
+        {
+        }
+
+        public DbSet<Transacao> Transacoes { get; set; }
+        public DbSet<ChaveModel> Chaves { get; set; }
+        public DbSet<Pagador> Pagadores { get; set; }
+        public DbSet<Recebedor> Recebedores { get; set; }
+    }
+}
