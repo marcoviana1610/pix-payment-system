69a86c6559c718b278f8525ff9091488682b9165
“marcoviana1610” <marcoviana1610@gmail.com>
2023-11-02 11:35:49 -0300

    testing post method

--- DesafioDotnet.API/Controllers/ChaveController.cs
+++ DesafioDotnet.API/Controllers/ChaveController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DesafioDotnet.Business.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DesafioDotnet.API.Controllers
 {
 
 
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class ChaveController : ControllerBase
     {
+        [HttpPost]
+        public ActionResult Post(ChaveModel chave) 
+        {
+            try
+            {
+                return Ok("Chave pix adicionada com sucesso");
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
     }
 }
